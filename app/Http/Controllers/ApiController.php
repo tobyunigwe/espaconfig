37566bc640651f4ad098afa751a42ac55dd08be7
Glynairo Kortijn <s1147464@student.windesheim.nl>
2020-12-23 14:08:30 +0100

    OAuth2.0 authentication & env. keys added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,12 +10,18 @@ use League\OAuth2\Client\Provider\GenericProvider;
 use Spatie\ArrayToXml\ArrayToXml;
 use App\Models\Configuration;
 use Illuminate\Http\Request;
+use TheNetworg\OAuth2\Client\Provider\Azure;
 
+/**
+ * Class ApiController
+ * @package App\Http\Controllers
+ */
 class ApiController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Client\PendingRequest
      */
 
@@ -27,63 +33,67 @@ class ApiController extends Controller
 //        return response()->json($response, 200);
 //    }
 
+    /**
+     * Handler for the incoming request from the Configuration Server.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+
     public function handleIncomingRequest(Request $request)
     {
         //GET IP adress (as identifier) from API call
         $ipAdress = $request->input('identifier');
 
         //GET configuration from VS and parse to json
-        $configuration = $this->authenticate()
-            ->get("https://configuration.picasse.io/Identifier/Configuration?route=espasdr&identifier=" . $ipAdress . "&content=true")
+        $unconverted = $this->authenticate()
+            ->get("https://configuration.picasse.io/Identifier/Configuration/espasdr/" . $ipAdress . "?content=true")
             ->json();
 
-        //convert the json data to xml
-        $data = collect($configuration)->map(function ($value) {
-            if (is_array($value)) {
-                return collect($value)->filter()->toArray();
-            }
+        //Return failure message
+        if (!$unconverted) {
+            $response = ApiHelpers::apiResponse(true, 400, 'Receiving configuration from Configuration Server failed (not found)', null);
+            return response()->json($response, 400);
+        } else {
 
-            return $value;
-        })->filter()->toArray();
+            //convert to XML when found
+            $configuration = $this->convertToXml($unconverted);
+//            $response = ApiHelpers::apiResponse(false, 200, 'Successfully received configuration from Configuration Server', $configuration);
+//            return response()->json($response, 200);
 
-        $result = ArrayToXml::convert($data, 'config', true, 'UTF-8');
+            //call method to connect to MC with SSH
+            app('App\Http\Controllers\SshController')->connection($ipAdress);
 
-        //opening source file, replacing it with $result
-        $file = fopen(env('SSH_SOURCE_FILE'), 'w+');
-        fwrite($file, $result);
-        fclose($file);
-
-        //call method to connect to MC with SSH
-        app('App\Http\Controllers\SshController')->connection($ipAdress);
-
-        return response($result)->header('Content-Type', 'text/xml');
-
+            return $configuration;
+        }
     }
 
 
-
-
+    /**
+     * authenticator method to receive a bearer token from Azure OAuth2.0
+     *
+     * @return \Illuminate\Http\Client\PendingRequest
+     */
     public function authenticate()
     {
-
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId' => 'cd464722-011c-4371-9566-7662c5ca1e87',    // The client ID assigned to you by the provider
-            'clientSecret' => 'hUlKc91n-5slnJ~t3HfKFiw1r3x_x~We.y',    // The client password assigned to you by the provider
-            'redirectUri' => '127.0.0.1:8000',
-            'urlAuthorize' => 'https://login.microsoftonline.com/25f2f5dc-0e4d-4c27-b5b4-ff4b5bf90d00/oauth2/v2.0/authorize',
-            'urlAccessToken' => 'https://login.microsoftonline.com/25f2f5dc-0e4d-4c27-b5b4-ff4b5bf90d00/oauth2/v2.0/token',
-            'urlResourceOwnerDetails' => '',
-            'scopes' => '6b093944-8625-4f1a-b861-029380fb4424/.default'
+        $provider = new Azure([
+            'clientId' => env('AZURE_CLIENT_ID'),
+            'clientSecret' => env('AZURE_CLIENT_SECRET'),
+            'tenant' => env('AZURE_TENANT'),
+            'urlAuthorize' => env('AZURE_URL_AUTHORIZE'),
+            'urlAccessToken' => env('AZURE_ACCESS_TOKEN'),
+            'defaultEndPointVersion' => Azure::ENDPOINT_VERSION_2_0
         ]);
+
 
         try {
 
             // Try to get an access token using the client credentials grant.
-            $accessToken = $provider->getToken();
+            $accessToken = $provider->getAccessToken('client_credentials', ['scope' => '6b093944-8625-4f1a-b861-029380fb4424/.default'])->getToken();
 
-        } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+        } catch (IdentityProviderException $e) {
 
-            // Failed to get the access token
+            // If it fails
             exit($e->getMessage());
         }
 
@@ -92,22 +102,17 @@ class ApiController extends Controller
         return $auth;
     }
 
-    public function index()
+
+    /**
+     * Converter method to convert the data from the Configuration server to XML.
+     *
+     * @param $unconverted
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function convertToXml($unconverted)
     {
-        $configurations = $this->authenticate()
-            ->get("https://configuration.picasse.io/devices/espasdr?subTree=true")
-            ->json();
-
-        dump($configurations);
-
-        return view('api.index', ['configurations' => $configurations]);
-    }
-
-    public function show($id)
-    {
-        $json = $this->authenticate()->get('https://configuration.picasse.io/Identifier/Configuration?route=espasdr&identifier=' . $id . '&content=true')->json();
-
-        $data = collect($json)->map(function ($value) {
+        //convert the json data to xml
+        $configuration = collect($unconverted)->map(function ($value) {
             if (is_array($value)) {
                 return collect($value)->filter()->toArray();
             }
@@ -115,8 +120,44 @@ class ApiController extends Controller
             return $value;
         })->filter()->toArray();
 
-        $result = ArrayToXml::convert($data, 'config', true, 'UTF-8');
+        $result = ArrayToXml::convert($configuration, 'config', true, 'UTF-8');
+
+        //opening source file, replacing it with $result
+        $file = fopen(env('SSH_SOURCE_FILE'), 'w+');
+        fwrite($file, $result);
+        fclose($file);
+
 
         return response($result)->header('Content-Type', 'text/xml');
+
     }
 }
+
+////    public function index()
+////    {
+////        $configurations = $this->authenticate()
+////            ->get("https://configuration.picasse.io/devices/espasdr?subTree=true")
+////            ->json();
+////
+////        dump($configurations);
+////
+////        return view('api.index', ['configurations' => $configurations]);
+////    }
+////
+////    public function show($id)
+////    {
+////        $json = $this->authenticate()->get('https://configuration.picasse.io/Identifier/Configuration?route=espasdr&identifier=' . $id . '&content=true')->json();
+////
+////        $data = collect($json)->map(function ($value) {
+////            if (is_array($value)) {
+////                return collect($value)->filter()->toArray();
+////            }
+////
+////            return $value;
+////        })->filter()->toArray();
+////
+////        $result = ArrayToXml::convert($data, 'config', true, 'UTF-8');
+////
+////        return response($result)->header('Content-Type', 'text/xml');
+////    }
+//}
